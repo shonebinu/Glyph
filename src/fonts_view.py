a7cc9aff6b038eba2a9895b7b210fd11cb00d741
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2026-01-17 11:27:54 +0530

    switch to signal factory

--- src/fonts_view.py
+++ src/fonts_view.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import List
-from gi.repository import Adw, Gtk, PangoCairo, GObject, Gio
+from gi.repository import Adw, Gtk, PangoCairo, GObject, Gio, Pango
 from .fonts_manager import FontMetadata
 
 
@@ -26,8 +26,49 @@ class FontsView(Adw.NavigationPage):
         self.private_font_map = PangoCairo.FontMap.new()
 
     def load_preview_fonts(self, file_path: Path):
+        # TODO: make this async and then show loading state, after that load fonts
         self.private_font_map.add_font_file(str(file_path))
 
     def show_fonts(self, fonts: List[FontMetadata]):
         items = [FontItem(font.family) for font in fonts]
         self.list_store.splice(0, self.list_store.get_n_items(), items)
+
+    @Gtk.Template.Callback()
+    def on_setup(self, _, list_item):
+        box = Gtk.Box(
+            orientation=Gtk.Orientation.VERTICAL,
+            spacing=8,
+            margin_top=8,
+            margin_bottom=8,
+            margin_start=8,
+            margin_end=8,
+        )
+
+        family_label = Gtk.Label(halign=Gtk.Align.START)
+        preview_label = Gtk.Label(
+            halign=Gtk.Align.START, ellipsize=Pango.EllipsizeMode.END
+        )
+
+        # TODO: apply this in list view for faster
+        preview_label.set_font_map(self.private_font_map)
+
+        box.append(family_label)
+        box.append(preview_label)
+        list_item.set_child(box)
+
+    @Gtk.Template.Callback()
+    def on_bind(self, _, list_item):
+        item = list_item.get_item()
+        box = list_item.get_child()
+
+        family_label = box.get_first_child()
+        preview_label = box.get_last_child()
+
+        family_label.set_text(item.family)
+        markup = (
+            f"<span font_family='{item.family}' size='xx-large'>"
+            "The quick brown fox jumps over the lazy dog."
+            "</span>"
+        )
+
+        preview_label.set_markup(markup)
