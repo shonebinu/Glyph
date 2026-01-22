efc4ff9a3696f684437b147b9c0a5a2bbab69bac
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2026-01-22 11:53:37 +0530

    move to blp

--- src/fonts_view.py
+++ src/fonts_view.py
@@ -7,9 +7,15 @@ from .fonts_manager import FontMetadata
 class FontItem(GObject.Object):
     __gtype_name__ = "FontItem"
 
+    family = GObject.Property(type=str)
+
     def __init__(self, family):
         super().__init__()
         self.family = family
+
+    @GObject.Property(type=str)
+    def preview_markup(self):
+        return f'<span font_family="{self.family}" size="x-large" fallback="false">The quick brown fox jumps over the lazy dog.</span>'
 
 
 @Gtk.Template(resource_path="/io/github/shonebinu/Glyph/fonts_view.ui")
@@ -31,59 +37,3 @@ class FontsView(Adw.NavigationPage):
     def show_fonts(self, fonts: List[FontMetadata]):
         items = [FontItem(font.family) for font in fonts]
         self.list_store.splice(0, self.list_store.get_n_items(), items)
-
-    @Gtk.Template.Callback()
-    def on_setup(self, _, list_item):
-        main_box = Gtk.Box(
-            orientation=Gtk.Orientation.HORIZONTAL,
-            spacing=12,
-            margin_top=12,
-            margin_bottom=6,
-            margin_start=12,
-            margin_end=24,
-        )
-
-        text_column = Gtk.Box(
-            orientation=Gtk.Orientation.VERTICAL,
-            spacing=2,
-            hexpand=True,
-        )
-
-        family_label = Gtk.Label(
-            halign=Gtk.Align.START,
-        )
-
-        text = "The quick brown fox jumps over the lazy dog."
-        preview_ins = Gtk.Inscription(
-            text=text,
-            height_request=72,
-            nat_chars=len(text),
-            nat_lines=1,
-            text_overflow=Gtk.InscriptionOverflow.ELLIPSIZE_END,
-        )
-
-        text_column.append(family_label)
-        text_column.append(preview_ins)
-
-        install_btn = Gtk.Button(
-            label="Install",
-            valign=Gtk.Align.CENTER,
-        )
-
-        main_box.append(text_column)
-        main_box.append(install_btn)
-
-        list_item.set_child(main_box)
-
-        list_item.family_label = family_label
-        list_item.preview_ins = preview_ins
-        list_item.install_btn = install_btn
-
-    @Gtk.Template.Callback()
-    def on_bind(self, _, list_item):
-        item = list_item.get_item()
-
-        list_item.family_label.set_text(item.family)
-        list_item.preview_ins.set_markup(
-            f'<span font_family="{item.family}" size="x-large" fallback="false">The quick brown fox jumps over the lazy dog.</span>'
-        )
