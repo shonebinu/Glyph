c612686d7e9bfb99581a5c0cc27d86aec076e1a5
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2026-01-27 07:25:45 +0530

    add fallback false

--- src/fonts_view.py
+++ src/fonts_view.py
@@ -121,7 +121,7 @@ class FontsView(Gtk.ScrolledWindow):
 
         list_item.family_label.set_text(item.display_name)
         list_item.preview_ins.set_markup(
-            f'<span font_family="{item.preview_family}" size="x-large">{item.preview_string}</span>'
+            f'<span font_family="{item.preview_family}" size="x-large" fallback="false">{item.preview_string}</span>'
         )
         list_item.category_label.set_text(item.category)
 
