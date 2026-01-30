5d1dad2d115aaabf8a51696d1ddfe59001175b91
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2026-01-30 15:34:05 +0530

    save

--- src/window.py
+++ src/window.py
@@ -1,6 +1,7 @@
 from gi.repository import Adw, Gtk
 from .fonts_manager import FontsManager
 from .fonts_view import FontsView
+import asyncio
 
 
 @Gtk.Template(resource_path="/io/github/shonebinu/Glyph/window.ui")
@@ -11,8 +12,11 @@ class GlyphWindow(Adw.ApplicationWindow):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        asyncio.create_task(self.setup())
 
+    async def setup(self):
         self.fonts_manager = FontsManager()
+        await self.fonts_manager.load_preview_fonts()
         self.fonts_view.set_fonts_manager(self.fonts_manager)
 
         # TODO: implement smart search
