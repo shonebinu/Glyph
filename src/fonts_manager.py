ade162042b181cd0994ed07a48132ba276492caf
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2026-02-23 21:17:16 +0530

    Listen user font dir for any changes

--- src/fonts_manager.py
+++ src/fonts_manager.py
@@ -38,13 +38,21 @@ class FontsManager:
         self.httpx_client = httpx.AsyncClient()
 
         data_dir = Path("/app/share/glyph")
-        fonts, categories, subsets = self.prepare_font_data(
+        fonts, categories, subsets, self.family_model_map = self.prepare_font_data(
             data_dir / "fonts.json", data_dir / "previews"
         )
 
         self.font_store.splice(0, 0, fonts)
         self.available_categories.splice(0, 0, categories)
         self.available_subsets.splice(0, 0, subsets)
+
+        self.user_font_dir.mkdir(parents=True, exist_ok=True)
+        self.installed_fonts_json_path.parent.mkdir(parents=True, exist_ok=True)
+
+        self.user_font_dir_monitor = Gio.File.new_for_path(
+            str(self.user_font_dir)
+        ).monitor_directory(Gio.FileMonitorFlags.NONE)
+        self.user_font_dir_monitor.connect("changed", self.on_user_font_dir_changed)
 
         # To sync real-time system fonts changes with fontmap
         settings = Gtk.Settings.get_default()
@@ -66,13 +74,13 @@ class FontsManager:
         return {
             family: dir_name
             for family, dir_name in raw_installed_fonts.items()
-            if (self.user_font_dir / dir_name).exists()
+            if (self.user_font_dir / dir_name).is_dir()
             and any((self.user_font_dir / dir_name).iterdir())
         }
 
     def prepare_font_data(
         self, fonts_json_path: Path, preview_files_path: Path
-    ) -> Tuple[List[FontModel], List[str], List[str]]:
+    ) -> Tuple[List[FontModel], List[str], List[str], Dict[str, FontModel]]:
         raw_data = json.loads(fonts_json_path.read_text())
 
         failed_families = self.load_custom_fonts(
@@ -82,25 +90,25 @@ class FontsManager:
         fonts = []
         avail_cats: Set[str] = set()
         avail_subs: Set[str] = set()
+        family_model_map = {}
 
         for font_dict in raw_data:
             family = font_dict["family"]
-
-            fonts.append(
-                FontModel(
-                    font_dict,
-                    is_installed=family in self.installed_fonts,
-                    is_preview_font_added=family not in failed_families,
-                )
+            model = FontModel(
+                font_dict,
+                is_installed=family in self.installed_fonts,
+                is_preview_font_added=family not in failed_families,
             )
 
+            fonts.append(model)
             avail_cats.update(font_dict.get("category", []))
             avail_subs.update(font_dict.get("subsets", []))
+            family_model_map[family] = model
 
         categories = ["All"] + sorted(list(avail_cats))
         subsets = ["All"] + sorted(list(avail_subs))
 
-        return fonts, categories, subsets
+        return fonts, categories, subsets, family_model_map
 
     def load_custom_fonts(
         self,
@@ -132,14 +140,14 @@ class FontsManager:
         )
 
     def is_font_outside_installed(self, family: str) -> bool:
-        return family in {f.get_name() for f in self.default_font_map.list_families()}
+        return self.default_font_map.get_family(family) is not None
 
     async def remove_font(self, font: FontModel):
         try:
             dir_name = self.installed_fonts[font.family]
 
             path = self.user_font_dir / dir_name
-            if path.exists():
+            if path.is_dir():
                 await asyncio.to_thread(shutil.rmtree, path)
 
             self.installed_fonts.pop(font.family)
@@ -152,9 +160,6 @@ class FontsManager:
     async def install_font(self, font: FontModel):
         try:
             font.is_installing = True
-
-            self.user_font_dir.mkdir(parents=True, exist_ok=True)
-            self.installed_fonts_json_path.parent.mkdir(parents=True, exist_ok=True)
 
             font_destination_path = (
                 self.user_font_dir / f"{font.family}_{str(uuid.uuid4())}"
@@ -199,3 +204,44 @@ class FontsManager:
             async with await anyio.open_file(path, "wb") as f:
                 async for chunk in resp.aiter_bytes(chunk_size=256 * 1024):
                     await f.write(chunk)
+
+    def on_user_font_dir_changed(
+        self,
+        monitor: Gio.FileMonitor,
+        file: Gio.File,
+        other_file: Gio.File,
+        event_type: Gio.FileMonitorEvent,
+    ):
+        if event_type in (
+            Gio.FileMonitorEvent.DELETED,
+            Gio.FileMonitorEvent.CHANGES_DONE_HINT,
+        ):
+            changed_dir = file.get_basename()
+
+            family = next(
+                (
+                    family
+                    for family, dir in self.installed_fonts.items()
+                    if dir == changed_dir
+                ),
+                None,
+            )
+
+            if not changed_dir or not family:
+                return
+
+            changed_dir_path = self.user_font_dir / changed_dir
+
+            if not changed_dir_path.is_dir() or not any(changed_dir_path.iterdir()):
+                self.installed_fonts.pop(family)
+                self.update_model_installed_status(family, False)
+
+    def update_model_installed_status(self, family: str, is_installed: bool):
+        model = self.family_model_map.get(family)
+
+        if not model:
+            return
+
+        if model.is_installed != is_installed:
+            print(model.family)
+            model.is_installed = is_installed
